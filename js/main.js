f82c9180731636320b8d28d88fc798e2e06269be
andrea9llo <andrea.novello93@gmail.com>
2019-11-06 17:45:48 +0100

    ex con contatore

--- js/main.js
+++ js/main.js
@@ -1,8 +1,24 @@
 // 15 di questi quadratini (scelti a caso all’inizio) se cliccati diventano rossi,
 // gli altri diventano verdi (tutti i rimanennti)
-$(".quadratini").click(function() {
-  $(this).css("background","green");
-})
-$(".rosso").click(function() {
-  $(this).css("background","red");
+
+$( document ).ready(function() {
+  var contatoreVerde = 0;
+  var contatoreRosso = 0;
+
+  $(".verde").click(function() {
+    $(this).css("background","green");
+    contatoreVerde = contatoreVerde + 1;
+    // console.log('verde!', contatoreVerde)
+    document.getElementById('cont-verde').innerHTML = "le caselle verdi aperte sono " + contatoreVerde;
+  })
+  $(".rosso").click(function() {
+    $(this).css("background","red");
+    contatoreRosso += 1;
+    // console.log("rosso!",contatoreRosso);
+    document.getElementById('cont-rosso').innerHTML = "le caselle rosse aperte sono " + contatoreRosso;
+  })
+  document.getElementById('cont-rosso').innerHTML = "le caselle rosse aperte sono " + contatoreRosso;
+  document.getElementById('cont-verde').innerHTML = "le caselle verdi aperte sono " + contatoreVerde;
+
+
 })
